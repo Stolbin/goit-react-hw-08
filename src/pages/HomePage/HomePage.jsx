9edaa8cdb5e9d4c7e0edc552c08c7d59115d649b
Stolbin <109080579+Stolbin@users.noreply.github.com>
2024-05-09 17:27:49 +0300

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,22 @@
 import { Helmet } from "react-helmet-async";
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
 import css from "./HomePage.module.css";
 
 const HomePage = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   return (
     <>
       <Helmet>
         <title>Home Page</title>
       </Helmet>
       <div>
-        <h1 className={css.text}>It is your personal phone book</h1>
+        {isLoggedIn ? (
+          <h1 className={css.text}>It is your personal phone book</h1>
+        ) : (
+          <h1 className={css.text}>Create your personal contact book</h1>
+        )}
       </div>
     </>
   );
